99a02552ffae5699cdc9f0ca6ca138d098d3988d
shan <shan@ideation.is>
2014-06-10 14:25:27 +0530

    ey open ports

--- cookbooks/sidekiq/attributes/default.rb
+++ cookbooks/sidekiq/attributes/default.rb
@@ -8,7 +8,7 @@ default[:sidekiq] = {
   # unless a utility name is set, in which case, Sidekiq will
   # only be installed on to a utility instance that matches
   # the name
-  :utility_name => 'Redis',
+  :utility_name => 'redis',
   
   # Number of workers (not threads)
   :workers => 1,
