9d9657aeab71a8efd9a6f6ca2e3a86c870becf5d
shan <shan@ideation.is>
2014-06-13 18:06:48 +0530

    Added new crons

--- cookbooks/crontab/recipes/default.rb
+++ cookbooks/crontab/recipes/default.rb
@@ -13,4 +13,24 @@ if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
     user     'deploy'
     command  "cd /data/relatednoise_api/current && RAILS_ENV=staging bundle exec rake twitter:quick_realtime &"
   end
+
+  cron "instagram_process_realtime_notifications" do
+    minute   '*/2'
+    hour     '*'
+    day      '*'
+    month    '*'
+    weekday  '*'
+    user     'deploy'
+    command  "cd /data/relatednoise_api/current && RAILS_ENV=staging bundle exec rake instagram:data &"
+  end
+
+  cron "twitter_track_tags_realtime" do
+    minute   '*/5'
+    hour     '*'
+    day      '*'
+    month    '*'
+    weekday  '*'
+    user     'deploy'
+    command  "cd /data/relatednoise_api/current && RAILS_ENV=staging bundle exec rake poll:twitter_tags_real &"
+  end
 end
